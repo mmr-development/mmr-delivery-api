8c5959a492ce5bd709223247c5f49c45f293b669
Rokas Siaudvirtas <rokas.net.info@gmail.com>
2025-04-28 19:12:36 +0200

    add swagger documentation plugin

---
+++ src/plugins/swagger.ts
@@ -0,0 +1,38 @@
+import fp from 'fastify-plugin';
+import fastifySwaggerUI from '@fastify/swagger-ui';
+import fastifySwagger from '@fastify/swagger';
+
+export default fp(async function (server) {
+    /*
+    * Fastify plugin for serving Swagger.
+    *
+    * @see {@link https://github.com/fastify/fastify-swagger}
+    */
+    await server.register(fastifySwagger, {
+        hideUntagged: true,
+        openapi: {
+            info: {
+                title: 'Track n Trace API',
+                version: '1.0.0',
+            },
+            components: {
+                securitySchemes: {
+                    bearerAuth: {
+                        type: 'http',
+                        scheme: 'bearer',
+                        bearerFormat: 'JWT',
+                    },
+                },
+            },
+        }
+    });
+
+    /*
+    * Fastify plugin for serving Swagger UI.
+    *
+    * @see {@link https://github.com/fastify/fastify-swagger-ui}
+    */
+    await server.register(fastifySwaggerUI, {
+
+    });
+});
